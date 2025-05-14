6da339f74e1b62d7fb9ffb99902d6c6e1d179fc5
catatsumuri <catatsumuri@gmail.com>
2025-05-14 06:55:21 +0000

    feat: show login flash message with toast notifications using sonner

--- app/Http/Middleware/HandleInertiaRequests.php
+++ app/Http/Middleware/HandleInertiaRequests.php
@@ -51,6 +51,7 @@ class HandleInertiaRequests extends Middleware
                 'location' => $request->url(),
             ],
             'sidebarOpen' => $request->cookie('sidebar_state') === 'true',
+            'flashMessage' => session()->get('flashMessage', []),
         ];
     }
 }
--- resources/js/layouts/app-layout.tsx
+++ resources/js/layouts/app-layout.tsx
@@ -1,14 +1,29 @@
+import { Toaster } from '@/components/ui/sonner';
 import AppLayoutTemplate from '@/layouts/app/app-sidebar-layout';
 import { type BreadcrumbItem } from '@/types';
-import { type ReactNode } from 'react';
+import { usePage } from '@inertiajs/react';
+import { useEffect, type ReactNode } from 'react';
+import { toast } from 'sonner';
 
 interface AppLayoutProps {
   children: ReactNode;
   breadcrumbs?: BreadcrumbItem[];
 }
 
-export default ({ children, breadcrumbs, ...props }: AppLayoutProps) => (
-  <AppLayoutTemplate breadcrumbs={breadcrumbs} {...props}>
-    {children}
-  </AppLayoutTemplate>
-);
+export default function AppLayout({ children, breadcrumbs, ...props }: AppLayoutProps) {
+  const { flashMessage } = usePage().props;
+  useEffect(() => {
+    if (flashMessage) {
+      Object.entries(flashMessage).forEach(([type, message]) => {
+        toast[type as keyof typeof toast](message);
+      });
+    }
+  }, [flashMessage]);
+
+  return (
+    <AppLayoutTemplate breadcrumbs={breadcrumbs} {...props}>
+      {children}
+      <Toaster />
+    </AppLayoutTemplate>
+  );
+}
